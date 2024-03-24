e1c6c6e5249eb2e9569d4e7778695dbe29b05278
Your Name <nakuldesigner77@gmail.com>
2024-03-25 02:05:44 +0530

    play,pause,back,forward functionality added

--- script.js
+++ script.js
@@ -2,25 +2,30 @@
 
 
 var arr = [
-    {songName:"Starboy",url:"./songs/starboy.mp3",img:"./images/starboy.jpg"},
-    {songName:"Chitta",url:"./songs/Chitta.mp3",img:"./images/chitta.jpg"},
-    {songName:"Dhundhala",url:"./songs/Dhundhala.mp3",img:"./images/dhundla.webp"},
-    {songName:"Psycho",url:"./songs/psycho.mp3",img:"./images/psycho.webp"},
-    {songName:"Faasla ft.Hasan Raheem",url:"./songs/Faasla-ft-Hasan Raheem.mp3",img:"./images/Faasla.jpg"}
+    { songName: "Starboy", url: "./songs/starboy.mp3", img: "./images/starboy.jpg" },
+    { songName: "Chitta", url: "./songs/Chitta.mp3", img: "./images/chitta.jpg" },
+    { songName: "Dhundhala", url: "./songs/Dhundhala.mp3", img: "./images/dhundla.webp" },
+    { songName: "Psycho", url: "./songs/psycho.mp3", img: "./images/psycho.webp" },
+    { songName: "Faasla ft.Hasan Raheem", url: "./songs/Faasla-ft-Hasan Raheem.mp3", img: "./images/Faasla.jpg" }
 ]
 
 
 var allsongs = document.querySelector('#all-songs')
+var poster = document.querySelector("#left")
+var play = document.querySelector("#play");
+var back = document.querySelector("#back");
+var forward = document.querySelector("#forward");
 var audio = new Audio()
+var selectedSong = 0
 
 
-function main(){
+function main() {
     var clutter = ""
 
 
-arr.forEach((item,id)=>{
-    
-    clutter += `<div class="song-card" id=${id}>
+    arr.forEach((item, id) => {
+
+        clutter += `<div class="song-card" id=${id}>
     <div class="part-1">
         <img src=${item.img} alt="">
         <h2>${item.songName}</h2>
@@ -28,27 +33,67 @@ arr.forEach((item,id)=>{
     </div>
     <h6>3:56</h6> 
     </div>`
-})
+    })
 
-allsongs.innerHTML = clutter
-allsongs.addEventListener("click",(e)=>{
-
-const info = arr[e.target.id];
-const bgChanger = document.querySelector("#left").style.backgroundImage = `url(${info.img})`;
-audio.src = info.url
-audio.play()
-
-});
+    allsongs.innerHTML = clutter
+    audio.src = arr[selectedSong].url
+    poster.style.backgroundImage = `url(${arr[selectedSong].img})`;
 }
 
 main()
 
+allsongs.addEventListener("click", (e) => {
+
+    selectedSong = e.target.id;
+    main()
+    play.innerHTML = `<i class="ri-pause-line"></i>`
+    flag = 1
+    audio.play()
+
+});
 
 
-const Playbtn = document.querySelector("#play");
+var flag = 0
 
-Playbtn.addEventListener("click",() => {
-
-    audio.pause();
+play.addEventListener("click", () => {
+    if (flag == 0) {
+        play.innerHTML = `<i class="ri-pause-line"></i>`
+        main();
+        audio.play();
+        flag = 1
+       
+    } else{
+        play.innerHTML = `<i class="ri-play-large-fill"></i>`
+        main();
+        audio.pause();
+        flag = 0
+        
+    }
 
 })
+
+
+
+forward.addEventListener("click", () => {
+if (selectedSong < arr.length - 1){
+    selectedSong++
+    main();
+    audio.play()
+}else{
+    forward.style.opacity = 0.2
+}
+
+
+})
+
+back.addEventListener("click", () => {
+    if (selectedSong > 0){
+        selectedSong--
+        main();
+        audio.play()
+    }else{
+        back.style.opacity = 0.2
+    }
+    
+    
+    })
